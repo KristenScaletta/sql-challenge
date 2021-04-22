9076ce475a7dc1a04fa547ca7f08e7a30647fd23
KristenScaletta <78096475+KristenScaletta@users.noreply.github.com>
2021-04-22 16:19:05 -0500

    Update sql-challenge.sql

--- EmployeeSQL/sql-challenge.sql
+++ EmployeeSQL/sql-challenge.sql
@@ -68,3 +68,12 @@ CREATE TABLE "dept_manager" (
 	FOREIGN KEY ("dept_no") REFERENCES "departments"("dept_no")
 );
 
+---employee number, last name, first name, sex, and salary
+SELECT employees.emp_no,
+  employees.last_name,
+  employees.first_name,
+  employees.sex,
+  salaries.salary
+FROM employees
+INNER JOIN salaries ON
+employees.emp_no = salaries.emp_no;
