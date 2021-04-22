c604a42499ea1910734e51a22b9e16e97850c4aa
KristenScaletta <78096475+KristenScaletta@users.noreply.github.com>
2021-04-22 16:12:05 -0500

    Update sql-challenge.sql

--- EmployeeSQL/sql-challenge.sql
+++ EmployeeSQL/sql-challenge.sql
@@ -7,71 +7,64 @@ drop table if exists salaries;
 drop table if exists dept_manager;
 
 
-CREATE TABLE "departments" (
-    "dept_no" int   NOT NULL,
-    "dept_name" varchar   NOT NULL,
-    CONSTRAINT "pk_departments" PRIMARY KEY (
-        "dept_no"
-     )
-);
-
-CREATE TABLE "dept_emp" (
-    "dept_no" int   NOT NULL,
-    "emp_no" int   NOT NULL,
-    CONSTRAINT "pk_dept_emp" PRIMARY KEY (
-        "emp_no"
-     )
-);
-
-CREATE TABLE "employees" (
-    "emp_no" int   NOT NULL,
-    "emp_title_id" int   NOT NULL,
-    "birth_date" varchar   NOT NULL,
-    "first_name" varchar   NOT NULL,
-    "last_name" varchar   NOT NULL,
-    "sex" varchar   NOT NULL,
-    "hire_date" varchar   NOT NULL,
-    CONSTRAINT "pk_employees" PRIMARY KEY (
-        "emp_no"
-     )
-);
-
 CREATE TABLE "titles" (
-    "title_id" int   NOT NULL,
+    "title_id" varchar   NOT NULL,
     "title" varchar   NOT NULL,
-    CONSTRAINT "pk_titles" PRIMARY KEY (
+    PRIMARY KEY (
         "title_id"
      )
 );
 
+CREATE TABLE "departments" (
+    "dept_no" varchar   NOT NULL,
+    "dept_name" varchar   NOT NULL,
+    PRIMARY KEY ("dept_no")
+);
+
+CREATE TABLE "employees" (
+    "emp_no" int   NOT NULL,
+    "emp_title_id" varchar   NOT NULL,
+    "birth_date" date   NOT NULL,
+    "first_name" varchar   NOT NULL,
+    "last_name" varchar   NOT NULL,
+    "sex" varchar   NOT NULL,
+    "hire_date" date   NOT NULL,
+    PRIMARY KEY (
+        "emp_no"
+     ),
+	
+	FOREIGN KEY("emp_title_id") REFERENCES "titles" ("title_id")
+);
+
+CREATE TABLE "dept_emp" (
+    "emp_no" int   NOT NULL,
+	"dept_no" varchar   NOT NULL,
+    
+   		PRIMARY KEY (
+        "emp_no", "dept_no"
+     ),
+	FOREIGN KEY ("emp_no") REFERENCES "employees"("emp_no"),
+	FOREIGN KEY ("dept_no") REFERENCES "departments"("dept_no")
+  
+);
+
+
 CREATE TABLE "salaries" (
     "emp_no" int   NOT NULL,
     "salary" int   NOT NULL,
-    CONSTRAINT "pk_salaries" PRIMARY KEY (
+    PRIMARY KEY (
         "emp_no"
-     )
+     ),
+	FOREIGN KEY ("emp_no") REFERENCES "employees"("emp_no")
 );
 
 CREATE TABLE "dept_manager" (
-    "dept_no" int   NOT NULL,
+    "dept_no" varchar   NOT NULL,
     "emp_no" int   NOT NULL,
-    CONSTRAINT "pk_dept_manager" PRIMARY KEY (
-        "dept_no"
-     )
+    PRIMARY KEY (
+        "dept_no", "emp_no"
+     ),
+	FOREIGN KEY ("emp_no") REFERENCES "employees"("emp_no"),
+	FOREIGN KEY ("dept_no") REFERENCES "departments"("dept_no")
 );
-
-ALTER TABLE "dept_emp" ADD CONSTRAINT "fk_dept_emp_dept_no" FOREIGN KEY("dept_no")
-REFERENCES "departments" ("dept_no");
-
-ALTER TABLE "employees" ADD CONSTRAINT "fk_employees_emp_no" FOREIGN KEY("emp_no")
-REFERENCES "dept_emp" ("emp_no");
-
-ALTER TABLE "titles" ADD CONSTRAINT "fk_titles_title_id" FOREIGN KEY("title_id")
-REFERENCES "employees" ("emp_title_id");
-
-ALTER TABLE "salaries" ADD CONSTRAINT "fk_salaries_emp_no" FOREIGN KEY("emp_no")
-REFERENCES "employees" ("emp_no");
-
-ALTER TABLE "dept_manager" ADD CONSTRAINT "fk_dept_manager_dept_no" FOREIGN KEY("dept_no")
-REFERENCES "dept_emp" ("dept_no");
 
