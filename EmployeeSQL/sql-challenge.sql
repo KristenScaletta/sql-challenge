84601c4bed9c00cc59fe93d998b2095fcd7190fc
KristenScaletta <78096475+KristenScaletta@users.noreply.github.com>
2021-04-22 16:57:57 -0500

    Update sql-challenge.sql

--- EmployeeSQL/sql-challenge.sql
+++ EmployeeSQL/sql-challenge.sql
@@ -139,6 +139,7 @@ ORDER BY dept_emp.dept_no;
 
 --In descending order, list the frequency count of employee last names, i.e., how many employees share each last name.
 
-SELECT last_name, COUNT(last_name) AS "Last Name Frequency" 
+SELECT last_name, COUNT(last_name) AS "Frequency" 
 FROM employees
-GROUP BY last_name;
+GROUP BY last_name
+ORDER BY COUNT(last_name) DESC;
